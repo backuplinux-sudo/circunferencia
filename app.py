4b7658ac3dbbfa723641a1b1dcd82238d3c5adb6
teste <teste@teste.com>
2025-03-20 19:46:07 -0300

    corrigindo o valor de pi

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def comprimento(raio: float) -> float:
     Returns:
         float: valor do comprimento
     """
-    return round(2 * 3.15 * raio, 2)
+    return round(2 * 3.14 * raio, 2)
 
 print(comprimento(float(sys.argv[1])))
